63298b91b8ace80f0626aca7a7e876ece31f438e
VershaCoder <vershamishra01@gmail.co>
2024-03-02 18:24:56 +0530

    modify msg to trigger the build

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -16,7 +16,7 @@ public class JenkinsCiCdApplication {
         //Basant kumar hota
         //sarvindra kumar
        name= name.split(" ")[0];
-        return "Hi " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
+        return "Hi " + name + ". Congratulations !! you have successfully completed Jenkins CI/CD demo !";
     }
 
     public static void main(String[] args) {
